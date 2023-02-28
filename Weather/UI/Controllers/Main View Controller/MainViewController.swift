22f4e98554f337a1e34adfcbbd43f4695c2e2b16
Vladislav Movileanu <vladislavmovileanu@gmail.com>
2023-02-28 17:41:17 +0200

    WTR-1 Added project architecture

--- Weather/UI/Controllers/Main View Controller/MainViewController.swift
+++ Weather/UI/Controllers/Main View Controller/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Weather
 //
 //  Created by vladikkk on 28/02/2023.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
 }
-
