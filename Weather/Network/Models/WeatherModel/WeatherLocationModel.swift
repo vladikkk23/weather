3db492100198723ef1e21402bc713efa377a8fe5
Vladislav Movileanu <vladislavmovileanu@gmail.com>
2023-03-02 11:32:34 +0200

    WTR-6 Removed some unnecesary properties

--- Weather/Network/Models/WeatherModel/WeatherLocationModel.swift
+++ Weather/Network/Models/WeatherModel/WeatherLocationModel.swift
@@ -11,14 +11,10 @@ import Foundation
 struct WeatherLocationModel: Codable {
     let name, region, country: String
     let lat, lon: Double
-    let tzID: String
-    let localtimeEpoch: Double
-    let localtime: String
+    let localtime: Double
     
     enum CodingKeys: String, CodingKey {
         case name, region, country, lat, lon
-        case tzID = "tz_id"
-        case localtimeEpoch = "localtime_epoch"
-        case localtime
+        case localtime = "localtime_epoch"
     }
 }
